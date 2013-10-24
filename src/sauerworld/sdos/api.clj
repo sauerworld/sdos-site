1b616ea614f467b57451628fbb5f5f2f63c3ee1a
Michael Gaare <michael@clickscape.com>
2013-10-24 17:45:05 -0400

    Extended timeout on api

--- src/sauerworld/sdos/api.clj
+++ src/sauerworld/sdos/api.clj
@@ -14,7 +14,7 @@
   [action & params]
   (let [api-res (msg/request "queue/storage" {:action action :params params})
         timeout-exception (Throwable. "Error accessing database -- request timed out.")
-        response-map (deref api-res 1000 nil)]
+        response-map (deref api-res 2000 nil)]
     (if-not (nil? response-map)
       (let [{status :status response :response} response-map]
         (if (= :error status)
