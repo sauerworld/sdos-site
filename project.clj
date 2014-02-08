2a8e2d60497c49dd686e8d4e9562b7d60cc4b7e8
Michael Gaare <michael@clickscape.com>
2014-02-08 17:05:54 -0500

    Bump versions of deps

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojars.jcrossley3/tools.namespace "0.2.4.1"]
-                             [org.immutant/immutant "1.0.1"]
+                             [org.immutant/immutant "1.1.0"]
                              [ring-mock "0.1.5"]]
               :immutant {:nrepl-port 0}}}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -14,15 +14,15 @@
                  [clj-time "0.6.0"]
                  [clj-rss "0.1.3"]
                  [environ "0.4.0"]
-                 [com.draines/postal "1.11.0"]
-                 [compojure "1.1.5"]
-                 [enlive "1.1.4"]
-                 [markdown-clj "0.9.31"]
-                 [korma "0.3.0-RC5"]
-                 [org.clojure/java.jdbc "0.3.0-alpha5"]
-                 [sauerworld/cube2.crypto "0.9-SNAPSHOT"]
+                 [com.draines/postal "1.11.1"]
+                 [compojure "1.1.6"]
+                 [enlive "1.1.5"]
+                 [markdown-clj "0.9.41"]
+                 [korma "0.3.0-RC6"]
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [sauerworld/cube2.crypto "0.9.1-SNAPSHOT"]
                  [com.h2database/h2 "1.3.170"]
-                 [clojurewerkz/scrypt "1.0.0"]
-                 [com.novemberain/validateur "1.5.0"]]
+                 [clojurewerkz/scrypt "1.1.0"]
+                 [com.novemberain/validateur "1.7.0"]]
   :immutant {:init "sauerworld.sdos.core/initialize"
              :context-path "/"})
