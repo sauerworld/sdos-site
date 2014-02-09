2ef78c064b49a38d8b42de4613efa93b133d0f06
Michael Gaare <michael@clickscape.com>
2014-02-08 23:51:38 -0500

    Comment the db migration stuff out for now until rewritten in lobos

--- src/sauerworld/sdos/db.clj
+++ src/sauerworld/sdos/db.clj
@@ -1,7 +1,7 @@
 (ns sauerworld.sdos.db
   (:require [korma.db :as db]
             [clojure.java.jdbc :as sql]
-            [clojure.java.jdbc.ddl :as ddl]
+            ;;[clojure.java.jdbc.ddl :as ddl]
             [clojure.string :as str]))
 
 (defn create-h2-spec
@@ -10,6 +10,11 @@
           :naming {:keys str/lower-case
                    :fields str/upper-case}}))
 
+(defn create-pg-spec
+  [{:keys [host username password db]}]
+
+  )
+
 (defn create-db
   [resource-loc]
   (let [db-spec (create-h2-spec resource-loc)]
@@ -17,66 +22,68 @@
         db/create-db
         (assoc :config db-spec))))
 
-(defn create-articles-table
-  [db-spec]
-  (sql/db-do-commands db-spec
-    (ddl/create-table
-     :articles
-     [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
-     [:date :timestamp]
-     [:title "varchar"]
-     [:author "varchar"]
-     [:content "varchar"]
-     [:category "varchar"])))
+(comment
+  (defn create-articles-table
+    [db-spec]
+    (sql/db-do-commands db-spec
+                        (ddl/create-table
+                         :articles
+                         [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
+                         [:date :timestamp]
+                         [:title "varchar"]
+                         [:author "varchar"]
+                         [:content "varchar"]
+                         [:category "varchar"])))
 
-(defn create-users-table
-  [db-spec]
-  (sql/db-do-commands db-spec
-    (ddl/create-table
-     :users
-     [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
-     [:username "varchar"]
-     [:password "varchar"]
-     [:email "varchar"]
-     [:validation_key "varchar"]
-     [:validated "boolean"]
-     [:pubkey "varchar"]
-     [:created :timestamp]
-     [:admin "boolean"])))
+  (defn create-users-table
+    [db-spec]
+    (sql/db-do-commands db-spec
+                        (ddl/create-table
+                         :users
+                         [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
+                         [:username "varchar"]
+                         [:password "varchar"]
+                         [:email "varchar"]
+                         [:validation_key "varchar"]
+                         [:validated "boolean"]
+                         [:pubkey "varchar"]
+                         [:created :timestamp]
+                         [:admin "boolean"])))
 
-(defn create-tournaments-table
-  [db-spec]
-  (sql/db-do-commands db-spec
-    (ddl/create-table
-     :tournaments
-     [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
-     [:date :timestamp]
-     [:name "varchar"]
-     [:registration_open "boolean"])))
+  (defn create-tournaments-table
+    [db-spec]
+    (sql/db-do-commands db-spec
+                        (ddl/create-table
+                         :tournaments
+                         [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
+                         [:date :timestamp]
+                         [:name "varchar"]
+                         [:registration_open "boolean"])))
 
-(defn create-events-table
-  [db-spec]
-  (sql/db-do-commands db-spec
-    (ddl/create-table
-     :events
-     [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
-     [:tournament "integer"]
-     [:name "varchar"]
-     [:team_mode "boolean"])))
+  (defn create-events-table
+    [db-spec]
+    (sql/db-do-commands db-spec
+                        (ddl/create-table
+                         :events
+                         [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
+                         [:tournament "integer"]
+                         [:name "varchar"]
+                         [:team_mode "boolean"])))
 
-(defn create-registrations-table
-  [db-spec]
-  (sql/db-do-commands db-spec
-    (ddl/create-table
-     :registrations
-     [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
-     [:event "integer"]
-     [:user "integer"]
-     [:team "varchar"]
-     [:created :timestamp])
-    (ddl/create-index :uniquereg :registrations
-                      [:event :user] :unique)))
+  (defn create-registrations-table
+    [db-spec]
+    (sql/db-do-commands db-spec
+                        (ddl/create-table
+                         :registrations
+                         [:id "INTEGER PRIMARY KEY AUTO_INCREMENT"]
+                         [:event "integer"]
+                         [:user "integer"]
+                         [:team "varchar"]
+                         [:created :timestamp])
+                        (ddl/create-index :uniquereg :registrations
+                                          [:event :user] :unique)))
 
-(def tables
-  {:articles create-articles-table
-   :users create-users-table})
+  (def tables
+    {:articles create-articles-table
+     :users create-users-table})
+  )
