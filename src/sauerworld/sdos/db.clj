98403d92247359c0e5fe69ceea1ddf9cf5c11d4e
Michael Gaare <michael@clickscape.com>
2014-06-04 21:55:31 -0400

    Pooled data source config

--- src/sauerworld/sdos/db.clj
+++ src/sauerworld/sdos/db.clj
@@ -1,5 +1,40 @@
 (ns sauerworld.sdos.db
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource))
+
+(defn make-pool
+  "Create a connection pool from db spec."
+  [{:keys [classname subprotocol subname user password
+           excess-timeout idle-timeout minimum-pool-size maximum-pool-size
+           test-connection-query
+           idle-connection-test-period
+           test-connection-on-checkin
+           test-connection-on-checkout]
+    :or   {excess-timeout (* 30 60)
+           idle-timeout (* 3 60 60)
+           minimum-pool-size 3
+           maximum-pool-size 15
+           test-connection-query nil
+           idle-connection-test-period 0
+           test-connection-on-checkin false
+           test-connection-on-checkout false}
+    :as   spec}]
+  (doto (ComboPooledDataSource.)
+    (.setDriverClass classname)
+    (.setJdbcUrl (str "jdbc:" subprotocol ":" subname))
+    (.setUser user)
+    (.setPassword password)
+    ;; expire excess connections after N minutes of inactivity
+    (.setMaxIdleTimeExcessConnections excess-timeout)
+    ;; expire all connections after N minutes of inactivity:
+    (.setMaxIdleTime idle-timeout)
+    (.setMinPoolSize minimum-pool-size)
+    (.setMaxPoolSize maximum-pool-size)
+    (.setIdleConnectionTestPeriod idle-connection-test-period)
+    (.setTestConnectionOnCheckin test-connection-on-checkin)
+    (.setTestConnectionOnCheckout test-connection-on-checkout)
+    (.setPreferredTestQuery test-connection-query)))
+
 
 
 (defn postgres
