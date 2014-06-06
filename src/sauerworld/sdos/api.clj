ec529b35450903e1a6861d0b4a4e80dc6b67c311
Michael Gaare <michael@clickscape.com>
2014-06-05 22:53:45 -0400

    Remove immutant messaging refs
    Keep api ns functions stubbed out for the moment just so we can get a
    repl up

--- src/sauerworld/sdos/api.clj
+++ src/sauerworld/sdos/api.clj
@@ -1,23 +1,15 @@
-(ns sauerworld.sdos.api
-  (:require [immutant.messaging :as msg]))
+(ns sauerworld.sdos.api)
 
 (def api-queue
   "queue/storage")
 
 (defn start-api
   [& [queue]]
-  (let [queue (or queue api-queue)]
-    (msg/start queue)))
+  ;; no-op
+  nil)
 
 (defn request
   "Performs storage api requests. Throws Throwable on error."
   [action & params]
-  (let [api-res (msg/request "queue/storage" {:action action :params params})
-        timeout-exception (Throwable. "Error accessing database -- request timed out.")
-        response-map (deref api-res 2000 nil)]
-    (if-not (nil? response-map)
-      (let [{status :status response :response} response-map]
-        (if (= :error status)
-          (throw (Throwable. "Database access error. Unknown request."))
-          response))
-      (throw timeout-exception))))
+  ;; no-op
+  nil)
