1634a87bfcd7f69e693bcc9ecb8f30ea1f37bd61
Michael Gaare <michael@clickscape.com>
2014-06-05 23:28:06 -0400

    Remove more immutant cruft

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
                              [ring-mock "0.1.5"]
                              [criterium "0.4.3"]
                              [com.h2database/h2 "1.4.178"]]
-              :immutant {:nrepl-port 0}}
              :test {:dependencies [[com.h2database/h2 "1.4.178"]]}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
