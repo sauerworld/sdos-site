ef378cd073d9be9f4811fd18035c4ca4790f2463
Michael Gaare <michael@clickscape.com>
2014-06-07 16:24:02 -0400

    Util functions for ksb case conversion

--- src/sauerworld/sdos/utils.clj
+++ src/sauerworld/sdos/utils.clj
@@ -1,8 +1,18 @@
-(ns sauerworld.sdos.utils)
+(ns sauerworld.sdos.utils
+  (:require [camel-snake-kebab :as csk]))
 
+;; just use cond-> in future
 (defn assoc-if
   [m cond k v]
   (let [assoc-fn (if (vector? k) assoc-in assoc)]
     (if cond
       (assoc-fn m k v)
       m)))
+
+(defn map-keys [f m]
+  (letfn [(mapper [[k v]] [(f k) (if (map? v) (map-keys f v) v)])]
+    (into {} (map mapper m))))
+
+(defn to-underscore-keys
+  [m]
+  (map-keys csk/->snake_case m))
