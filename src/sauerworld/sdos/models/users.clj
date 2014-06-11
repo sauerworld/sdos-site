83291a4f69079967d68f643e272385cf66073ff0
Michael Gaare <michael@clickscape.com>
2014-06-11 01:43:52 -0400

    Users model

--- src/sauerworld/sdos/models/users.clj
+++ src/sauerworld/sdos/models/users.clj
@@ -1,137 +1,113 @@
 (ns sauerworld.sdos.models.users
   (:require [clojurewerkz.scrypt.core :as sc]
-            [clj-time.coerce :refer (to-date)]
+            [clj-time.coerce :as tc]
             [clj-time.core :refer (now)]
-            [validateur.validation :as v])
+            [sauerworld.sdos.model :as model]
+            [sqlingvo.core :refer (sql) :as sql]
+            [clojure.java.jdbc :as jdbc])
   (:import [java.util UUID]))
 
-(comment
-  (defn hash-password
-    [pw]
-    (sc/encrypt pw 16384 8 1))
+(defn hash-password
+  [pw]
+  (sc/encrypt pw 16384 8 1))
 
-  (defn check-password
-    [pw hash]
-    (sc/verify pw hash))
+(defn check-password
+  [user pw]
+  (sc/verify pw (:password user)))
 
-  (defn random-uuid
-    []
-    (str (java.util.UUID/randomUUID)))
+(defn random-uuid
+  []
+  (str (java.util.UUID/randomUUID)))
 
-  (defn base-users-query
-    [db]
-    (-> (k/create-entity "users")
-        (k/database db)))
+(def ^{:private true} key-spec
+  "Spec to convert User keys to db row."
+  {:validated? :validated
+   :admin? :admin})
 
-  (defn insert-user
-    [db {:keys [username password email admin
-                pubkey validation-key validated created]}]
-    (let [admin (true? admin)
-          pubkey (or pubkey nil)
-          validation-key (or validation-key (random-uuid))
-          validated (or validated false)
-          password (hash-password password)
-          created (or created (to-date (now)))]
-      (-> (base-users-query db)
-          (k/insert
-           (k/values {:username username
-                      :password password
-                      :email email
-                      :validation_key validation-key
-                      :validated validated
-                      :pubkey pubkey
-                      :created created
-                      :admin admin})))))
+(def ^{:private true} ->db-val-spec
+  "Spec to convert User vals to db row."
+  {:created-date tc/to-date})
 
-  (defn get-by-validation-key
-    [db validation-key]
-    (let [base (base-users-query db)]
-      (-> base
-          (k/select (k/where {:validation_key validation-key}))
-          first)))
+(def ^{:private true} ->user-val-spec
+  "Spec to convert db row vals to User."
+  {:created-date tc/to-date-time})
 
-  (defn set-validated
-    [db user]
-    (when-let [id (:id user)]
-      (-> (base-users-query db)
-          (k/update
-           (k/set-fields {"VALIDATED" true})
-           (k/where {:id id})))))
+(defrecord User
+    [id username password
+     validation-key validated? pubkey
+     created-date admin?]
 
-  (defn get-by-username
-    [db username]
-    (-> (base-users-query db)
-        (k/select (k/where {:username username}))
-        first))
+  model/DatabaseRecord
+  (create [this db]
+    (jdbc/execute! db
+                   (sql
+                    (sql/insert :users []
+                      (sql/values
+                       (model/->db this key-spec ->db-val-spec))))))
+  (read [this db]
+    {:pre [(or id username validation-key)]}
+    ;; uses id, username or validation-key
+    (let [query (sql (sql/select [*]
+                       (sql/from :users)
+                       (sql/limit 1)
+                       (sql/where
+                        (cond
+                         id '(= :id id)
+                         username '(= :username username)
+                         validation-key '(= :validation_key validation-key)))))]
+      (some-> (jdbc/execute! db query)
+              first
+              (model/->record key-spec ->user-val-spec true)
+              (->> (merge this)))))
+  (update [this db]
+      {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/update :users
+                        (-> this
+                            (model/->db key-spec ->db-val-spec)
+                            (dissoc :id))
+                      (sql/where '(= :id id))))))
+  (delete [this db]
+    {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/delete :users
+                      (sql/where '(= :id id)))))))
 
-  (defn check-login
-    [db username password]
-    (when-let [user (get-by-username db username)]
-      (when (check-password password (:password user))
-        user)))
+(defn user
+  "Creates a User record, either from a map or an id.
+   If no-hash? is true, password fields won't be hashed."
+  [user-or-id & [no-hash?]]
+  {:pre [(or (and (integer? user-or-id)
+                  (pos? user-or-id))
+             (map? user-or-id))]}
+  (if (integer? user-or-id)
+    (map->User {:id user-or-id})
+    (let [defaults {:created-date (now)
+                    :admin false}]
+      (-> user-or-id
+          (cond->
+           (and (not no-hash?)
+                (contains? user-or-id :password))
+           (update-in [:password] hash-password))
+          (->> (merge defaults))
+          map->User))))
 
-  (defn add-pubkey
-    [db user pubkey]
-    (when-let [id (:id user)]
-      (-> (base-users-query db)
-          (k/update
-           (k/set-fields {"PUBKEY" pubkey})
-           (k/where {:id id})))))
+(defn db->user
+  "Creates a User record from a database result."
+  [result]
+  (map->User (model/->record result key-spec ->user-val-spec true)))
 
-  (defn update-password
-    "Updates a user's password."
-    [db user new-password]
-    (when-let [id (:id user)]
-      (-> (base-users-query db)
-          (k/update
-           (k/set-fields {"PASSWORD" (hash-password new-password)})
-           (k/where {:id id})))))
+(defn update-password
+  "Updates a User record with a new password, hashing it."
+  [user new-password]
+  (assoc user :password (hash-password new-password)))
 
-  (defn match-of
-    "Creates validation function that specifies that two attributes must be the
-   same (for example, for password & password confirmation fields."
-    [attribute1 attribute2]
-    (let [getter (fn [k] (fn [m] (if (vector? k)
-                                   (get-in m k)
-                                   (get m k))))
-          f1 (getter attribute1)
-          f2 (getter attribute2)]
-      (fn [m]
-        (let [[v1 v2] ((juxt f1 f2) m)
-              res (= v1 v2)
-              msg (str "must match " attribute2)
-              errors (if res {} {attribute1 #{msg}})]
-          [(empty? errors) errors]))))
-
-  (defn uniqueness-of
-    "Validation function that checks that a value unique. Takes a key to retrieve
-   the value, and a function that, when passed the value, will report its
-   uniqueness. Like, for instance, a database query function."
-    [attribute check-fn]
-    (let [f (if (vector? attribute) get-in get)]
-      (fn [m]
-        (let [value (f m attribute)
-              unique? (check-fn value)
-              msg (str value " not available, already taken.")
-              errors (if unique? {} {attribute #{msg}})]
-          [(empty? errors) errors]))))
-
-  (def password-match
-    (match-of :password :password-confirm))
-
-  (defn make-registration-validator
-    [db]
-    (v/validation-set
-     (v/presence-of :email)
-     (v/presence-of :password)
-     password-match
-     (v/presence-of :username)
-     (uniqueness-of :username #(->> %
-                                    (get-by-username db)
-                                    nil?))))
-
-  (def password-validator
-    (v/validation-set
-     (v/presence-of :password)
-     password-match))
-  )
+(defn find-all-users
+  [db]
+  (some->> (sql
+            (sql/select [*]
+              (sql/from :users)))
+           (jdbc/execute! db)
+           (map db->user)))
