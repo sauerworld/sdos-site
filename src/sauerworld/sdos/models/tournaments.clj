7b3e0b9569263dbe889d689e0cf8e3a864bc9dc2
Michael Gaare <michael@clickscape.com>
2014-06-11 01:46:56 -0400

    Tournament / Event / Registration CRUD

--- src/sauerworld/sdos/models/tournaments.clj
+++ src/sauerworld/sdos/models/tournaments.clj
@@ -1,64 +1,216 @@
 (ns sauerworld.sdos.models.tournaments
-  (:require [clj-time.core :refer (date-time now minus days)]
-            [clj-time.coerce :refer (to-date to-date-time)]))
+  (:require [clj-time.core :refer (now)]
+            [clj-time.coerce :as tc]
+            [sauerworld.sdos.model :as model]
+            [sqlingvo.core :refer (sql) :as sql]
+            [clojure.java.jdbc :as jdbc]))
+
+(def ^{:private true} tournament-key-spec
+  "Spec to convert Tournament keys to db row."
+  {:registration-open? :registration-open})
+
+(def ^{:private true} tournament->db-val-spec
+  "Spec to convert Tournament vals to db row."
+  {:start-date tc/to-date
+   :end-date tc/to-date})
+
+(def ^{:private true} ->tournament-val-spec
+  {:start-date tc/to-date-time
+   :end-date tc/to-date-time})
+
+(defrecord Tournament
+    [id name start-date end-date registration-open?]
+
+  model/DatabaseRecord
+  (create [this db]
+    (jdbc/execute! db
+                   (sql
+                    (sql/insert :tournaments []
+                      (sql/values
+                       (model/->db this
+                                   tournament-key-spec
+                                   tournament->db-val-spec))))))
+  (read [this db]
+    {:pre [(or id (:date this))]}
+    ;; uses id or date (queries between start-date and end-date
+    (let [query (sql
+                 (sql/select [*]
+                   (sql/from :tournaments)
+                   (sql/limit 1)
+                   (sql/where
+                    (cond
+                     id '(= :id id)
+                     (:date this) '(> :end_date (:date this) :start_date)))))]
+      (some-> (jdbc/execute! db query)
+              first
+              (model/->record tournament-key-spec ->tournament-val-spec true)
+              (->> (merge this))
+              (dissoc :date))))
+  (update [this db]
+      {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/update :tournaments
+                        (-> this
+                            (model/->db tournament-key-spec
+                                        tournament->db-val-spec)
+                            (dissoc :id))
+                      (sql/where '(= :id id))))))
+  (delete [this db]
+    {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/delete :tournaments
+                      (sql/where '(= :id id)))))))
+
+(defn tournament
+  "Creates a Tournament record, either from a map or an id."
+  [tournament-or-id]
+  {:pre [(or (and (integer? tournament-or-id)
+                  (pos? tournament-or-id))
+             (map? tournament-or-id))]}
+  (map->Tournament (if (integer? tournament-or-id)
+                     {:id tournament-or-id}
+                     tournament-or-id)))
+
+(defn db->tournament
+  [result]
+  (map->Tournament (model/->record result tournament-key-spec
+                                   ->tournament-val-spec true)))
+
+(def ^{:private true} event-key-spec
+  {:team-mode? :team-mode})
+
+(def ^{:private true} event->db-val-spec
+  {})
+
+(def ^{:private true} ->event-val-spec
+  {})
+
+(defrecord Event
+    [id tournament-id name mode team-mode?]
+
+  model/DatabaseRecord
+  (create [this db]
+    {:pre [(integer? id) (integer? tournament-id)]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/insert :events []
+                      (sql/values
+                       (model/->db this
+                                   event-key-spec
+                                   event->db-val-spec))))))
+  (read [this db]
+    {:pre [id]}
+    (let [query (sql
+                 (sql/select [*]
+                   (sql/from :events)
+                   (sql/limit 1)
+                   (sql/where '(= :id id))))]
+      (some-> (jdbc/execute! db query)
+              first
+              (model/->record event-key-spec ->event-val-spec true)
+              (->> (merge this)))))
+  (update [this db]
+      {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/update :events
+                        (-> this
+                            (model/->db event-key-spec
+                                        event->db-val-spec)
+                            (dissoc :id))
+                      (sql/where '(= :id id))))))
+  (delete [this db]
+    {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/delete :events
+                      (sql/where '(= :id id)))))))
+
+(defn event
+  "Creates an Event record, either from a map or an id."
+  [event-or-id]
+  {:pre [(or (and (integer? event-or-id)
+                  (pos? event-or-id))
+             (map? event-or-id))]}
+  (map->Event (if (integer? event-or-id)
+                {:id event-or-id}
+                event-or-id)))
+
+(defn db->event
+  [result]
+  (map->Event (model/->record result event-key-spec
+                              ->event-val-spec true)))
+
+(def ^{:private true} registration-key-spec
+  {})
+
+(def ^{:private true} registration->db-val-spec
+  {:date tc/to-date})
+
+(def ^{:private true} ->registration-val-spec
+  {:date tc/to-date-time})
+
+(defrecord Registration
+    [id event-id user-id team date]
+
+  model/DatabaseRecord
+  (create [this db]
+    {:pre [(integer? id) (integer? event-id) (integer? user-id)]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/insert :registrations []
+                      (sql/values
+                       (model/->db this
+                                   registration-key-spec
+                                   registration->db-val-spec))))))
+  (read [this db]
+    {:pre [(or id (and event-id user-id))]}
+    (let [query (sql
+                 (cond->
+                  (sql/select [*]
+                    (sql/from :registrations)
+                    (sql/limit 1))
+                  id
+                  (sql/compose
+                   (sql/where '(= :id id)))
+                  (not id)
+                  (sql/compose
+                   (sql/where '(= :event_id event-id))
+                   (sql/where '(= :user_id user-id) :and))))]
+      (some-> (jdbc/execute! db query)
+              first
+              (model/->record registration-key-spec ->registration-val-spec true)
+              (->> (merge this)))))
+  (update [this db]
+      {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/update :registrations
+                        (-> this
+                            (model/->db registration-key-spec
+                                        registration->db-val-spec)
+                            (dissoc :id))
+                      (sql/where '(= :id id))))))
+  (delete [this db]
+    {:pre [id]}
+    (jdbc/execute! db
+                   (sql
+                    (sql/delete :registrations
+                      (sql/where '(= :id id)))))))
+
+(defn registration
+  "Creates a Registration record, either from a map or an id."
+  [registration-or-id]
+  {:pre [(or (and (integer? registration-or-id)
+                  (pos? registration-or-id))
+             (map? registration-or-id))]}
+  (map->Registration (if (integer? registration-or-id)
+                       {:id registration-or-id}
+                       registration-or-id)))
 
 (comment
-  (defn base-tournaments-query
-    [db]
-    (-> (k/create-entity "tournaments")
-        (k/database db)))
-
-  (defn base-events-query
-    [db]
-    (-> (k/create-entity "events")
-        (k/database db)))
-
-  (defn base-registrations-query
-    [db]
-    (-> (k/create-entity "registrations")
-        (k/database db)))
-
-  (defn insert-tournament
-    [db {:keys [name date registration-open] :as tournament}]
-    (let [registration-open (or registration-open false)
-          date (to-date date)
-          t-entity {:name name
-                    :date date
-                    :registration_open registration-open}]
-      (-> (base-tournaments-query db)
-          (k/insert
-           (k/values t-entity)))))
-
-  (defn insert-event
-    [db {:keys [tournament name team-mode]}]
-    (let [team-mode (or team-mode false)
-          tournament (if (number? tournament)
-                       (int tournament)
-                       (-> tournament :id int))]
-      (-> (base-events-query db)
-          (k/insert
-           (k/values {:name name
-                      :tournament tournament
-                      :team_mode team-mode})))))
-
-  (defn insert-registration
-    [db {:keys [event user team created]}]
-    (let [user (if (number? user)
-                 (int user)
-                 (-> user :id int))
-          event (if (number? event)
-                  (int event)
-                  (-> event :id int))
-          team (or team "")
-          created (if created
-                    (to-date created)
-                    (to-date (now)))]
-      (-> (base-registrations-query db)
-          (k/insert
-           (k/values {:event event
-                      :user user
-                      :team team
-                      :created created})))))
 
   (defn get-next-tournament
     [db & [date]]
@@ -68,25 +220,6 @@
            (k/where {:date [> date]})
            (k/order :date :asc))
           first)))
-
-  (defn get-tournament-by-id
-    [db id]
-    (-> (base-tournaments-query db)
-        (k/select
-         (k/where {:id id})
-         (k/limit 1))
-        first))
-
-  (defn get-current-tournament
-    [db & [date]]
-    (let [date (to-date-time (or date (now)))
-          yesterday (-> date
-                        (minus (days 1))
-                        to-date)]
-      (-> (base-tournaments-query db)
-          (k/select
-           (k/where {:date [> yesterday]})
-           (k/order :date :asc)))))
 
   (defn get-tournaments
     [db]
@@ -126,11 +259,5 @@
                    (= :users.id :user))
            (k/where {:events.tournament id})))))
 
-  (defn update-team
-    [db registration team]
-    (when-let [id (:id registration)]
-      (-> (base-registrations-query db)
-          (k/update
-           (k/set-fields {"team" team})
-           (k/where {:id id})))))
+
   )
