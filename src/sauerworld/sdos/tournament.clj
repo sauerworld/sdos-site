67e174e7031ec92e477446420e5f6302b8c4d585
Michael Gaare <michael.gaare@gmail.com>
2014-11-12 19:25:19 -0500

    Updates to tournament controller

--- src/sauerworld/sdos/tournament.clj
+++ src/sauerworld/sdos/tournament.clj
@@ -2,14 +2,19 @@
   (:require [sauerworld.sdos.api :refer (request)]
             [sauerworld.sdos.layout :refer (error-template app-page)]
             [sauerworld.sdos.settings :refer (get-settings)]
+            [sauerworld.sdos.system.app :as app]
+            [sauerworld.sdos.models.tournaments :as tournaments]
+            [sauerworld.sdos.models.events :as events]
+            [sauerworld.sdos.models.registrations :as registrations]
             [sauerworld.sdos.views.tournament :as view]))
 
 (defn show-tournament
   [req]
   (let [id (-> req :route-params :id Integer.)
         settings (get-settings req)
+        db (app/get-db req)
         user (-> req :session :user)]
-    (if-let [tourney (request :tournaments/get-tournament id :events :registrations :users)]
+    (if-let [tourney (tournaments/get-tournament-with db id :events :registrations :users)]
       (app-page settings (view/tournament tourney user))
       (error-template settings
                       (str  "Tournament " id " not found.")))))
@@ -17,71 +22,80 @@
 (defn show-next-tournament
   [req]
   (let [settings (get-settings req)
-        next-t (request :tournaments/get-current-tournament)
+        db (app/get-db req)
+        next-t (tournaments/find-next db)
         user (-> req :session :user)]
     (if next-t
-      (let [tourney (request :tournaments/get-tournament next-t :events :registrations :users)]
+      (let [tourney (tournaments/get-tournament-with db (:id next-t) :events :registrations :users)]
         (app-page settings (view/tournament tourney)))
       (error-template settings
                       "Next tournament not yet scheduled."))))
 
 (defn show-tournaments
   [req]
-  (let [settings (get-settings req)]
-    (if-let [tourneys (request :tournaments/get-tournaments :events)]
-      (app-page settings (view/show-tournaments tourneys))
+  (let [settings (get-settings req)
+        db (app/get-db req)]
+    (if-let [tourneys (tournaments/find-all db)]
+      (let [events (events/find-by-tournaments db tourneys)
+            tourneys-w-events (map (fn [t]
+                                     (tournaments/combine-tournament-entities {:tournament t :events events}))
+                                   tourneys)]
+        (app-page settings (view/show-tournaments tourneys-w-events)))
       (error-template settings
                       (str "No tournaments found.")))))
 
 (defn show-signup-page
   [req event-id user]
-  (let [settings (get-settings req)]
-    (if-let [event (request :tournaments/get-event-by-id event-id)]
-      (let [tournament (request :tournaments/get-tournament-for-event event)]
-        (if (:registration_open tournament)
+  (let [settings (get-settings req)
+        db (app/get-db req)]
+    (if-let [event (events/find-by-id db event-id)]
+      (let [tournament (tournaments/find-by-id db (:tournament-id event))]
+        (if (:registration-open tournament)
           (app-page settings
                     (view/signup-page (assoc event :tournament tournament)))
           (error-template settings "Registrations are not open for this event.")))
       (error-template settings "Event not found."))))
 
 (defn edit-signup-page
-  [req signup user]
-  (let [settings (get-settings req)]
-    (if-not (= (:user signup) (:id user))
+  [req registration user]
+  (let [settings (get-settings req)
+        db (app/get-db req)]
+    (if-not (= (:user-id registration) (:id user))
       (error-template settings "You do not have access to edit this registration.")
-      (let [event (request :tournaments/get-event-by-id (:event signup))
-            tournament (request :tournaments/get-tournament-for-event event)]
-        (if-not (:registration_open tournament)
+      (let [event (events/find-by-id db (:event-id registration))
+            tournament (tournaments/find-by-id db (:tournament-id event))]
+        (if-not (:registration-open tournament)
           (error-template settings "Registrations are not open for this event.")
           (app-page settings
                     (view/signup-page
-                     (assoc event :tournament tournament) signup)))))))
+                     (assoc event :tournament tournament) registration)))))))
 
 (defn event-signup
   [req]
   (let [user (-> req :session :user)
-        event-id (-> req :route-params :id Integer.)]
-    (if-let [signup (-> (request :tournaments/get-event-signups event-id user)
-                        first)]
-      (edit-signup-page req signup user)
+        event-id (-> req :route-params :id Integer.)
+        db (app/get-db req)]
+    (if-let [registration (registrations/find-by-user-and-event db (:id user) event-id)]
+      (edit-signup-page req registration user)
       (show-signup-page req event-id user))))
 
 (defn do-event-signup
   [req]
   (let [settings (get-settings req)
-        user (-> req :session :user)
+        user-id (-> req :session :user :id Integer/parseInt)
         event-id (-> req :route-params :id Integer.)
-        event (request :tournaments/get-event-by-id event-id)
-        dups? (first (request :tournaments/get-event-signups event-id user))
-        tournament (request :tournaments/get-tournament-for-event event)]
-    (if-not (:registration_open tournament)
+        db (app/get-db req)
+        event (events/find-by-id db event-id)
+        dup? (registrations/find-by-user-and-event db user-id event-id)
+        tournament (tournaments/find-by-id db (:tournament-id event))]
+    (if-not (:registration-open tournament)
       (error-template settings "Registrations are not open for this event.")
-      (if dups?
+      (if dup?
         (error-template settings "You have aleady signed up for this event.")
-        (let [signup {:event event-id
-                      :user (:id user)
-                      :team (-> req :params :team)}]
-          (if (request :tournaments/insert-registration signup)
+        (let [registration {:event-id event-id
+                            :user-id user-id
+                            :team (-> req :params :team)}]
+          (if (registrations/create db registration)
             (app-page settings "Registration successful!")
             (error-template settings
                             "Registration failed. Please try again later.")))))))
@@ -90,21 +104,21 @@
   [req]
   (let [settings (get-settings req)
         event-id (-> req :route-params :id Integer.)
-        signup-id (-> req :route-params :s-id Integer.)
-        user (-> req :session :user)]
-    (if-let [event (request :tournaments/get-events-by-id event-id)]
-      (if-not (:team_mode event)
+        registration-id (-> req :route-params :s-id Integer.)
+        user-id (-> req :session :user :id Integer/parseInt)
+        db (app/get-db req)]
+    (if-let [event (events/find-by-id db event-id)]
+      (if-not (:team-mode event)
         (error-template settings "This is not a team mode - nothing to edit.")
-        (let [tournament (request :tournaments/get-tournament-by-event event)]
-          (if-not (:registration_open tournament)
+        (let [tournament (tournaments/find-by-id db (:tournament-id event))]
+          (if-not (:registration-open tournament)
             (error-template settings "Registrations are not open for this event.")
-            (if-let [signup (->
-                             (request :tournaments/get-event-signup signup-id user)
-                             first)]
-              (if-not (= (:id user) (:user signup))
+            (if-let [registration (registrations/find-by-user-and-event db user-id event-id)]
+              (if-not (= user-id (:user-id registration))
                 (error-template settings "You do not have permission to edit this registration.")
                 (let [team (-> req :params :team)]
-                  (if (request :tournaments/udpate-team signup team)
+                  (if (registrations/update db {:id (:id registration)
+                                                :team team})
                     (app-page settings "Edit successful.")
                     (error-template settings "Unable to edit your registration. Please try again later."))))
               (error-template settings "Invalid signup.")))))
@@ -114,17 +128,16 @@
   [req]
   (let [settings (get-settings req)
         event-id (-> req :route-params :id Integer.)
-        user (-> req :session :user)]
-    (if-let [event (request :tournaments/get-event-by-id event-id)]
-      (let [tournament (request :tournaments/get-tournament-for-event event)]
-        (if-not (:registration_open tournament)
+        user-id (-> req :session :user :id Integer/parseInt)
+        db (app/get-db req)]
+    (if-let [event (events/find-by-id db event-id)]
+      (let [tournament (tournaments/find-by-id db (:tournament-id event))]
+        (if-not (:registration-open tournament)
           (error-template settings "Event registration is closed for this tournament. You cannot delete your signup.")
-          (let [signup (->
-                        (request :tournaments/get-event-signups event-id user)
-                        first)]
-            (if-not (= (:id user) (:user signup))
+          (let [registration (registrations/find-by-user-and-event db user-id event-id)]
+            (if-not (= user-id (:user-id registration))
               (error-template settings "You do not have permission to delete this signup.")
               (do
-                (request :tournaments/delete-registration signup)
+                (registrations/delete db registration)
                 (app-page settings "Signup deleted."))))))
       (error-template settings "Unknown event, cannot delete signup."))))
